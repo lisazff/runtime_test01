eeb5a3c6816ff2316b566925a4c02f70a5140bf0
lisa <1085983505@qq.com>
2017-06-01 00:19:15 +0800

    Initial Commit

---
+++ runtime_test01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime_test01
+//
+//  Created by lisa on 2017/6/1.
+//  Copyright © 2017年 lisa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime_test01/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  runtime_test01
+//
+//  Created by lisa on 2017/6/1.
+//  Copyright © 2017年 lisa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
